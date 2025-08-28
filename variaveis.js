c9a0cdd00b9e3f4d97d5bcd38865480664510a48
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-08-28 18:59:03 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+
+const PI = 3.14;
+console.log("Value of PI:", PI);
+
+let radius = 5;
+let area = PI * radius * radius;
+console.log("Area of circle with radius", radius, "is:", area);
+
+let nome = "Mathues"
+
+
+console.log("............");
+
+console.log(typeof(PI));
+console.log(typeof(radius));
+console.log(typeof(area));
+console.log(typeof(nome));
